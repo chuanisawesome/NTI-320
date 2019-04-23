e520c416a7ae3b2a62cd22be51c66e65ca6e2f54
chuanisawesome <cchang30@seattlecentral.edu>
2019-04-23 15:52:25 -0700

    Update nagios_client_install.sh

--- lab1_nagios/nagios_client_install.sh
+++ lab1_nagios/nagios_client_install.sh
@@ -11,12 +11,28 @@ yum -y install httpd
 systemctl enable httpd
 systemctl start httpd
 
-yum -y install nagios-nrpe-server nagios-plugins
+#####INSTALL PLUG-INS#####
+yum -y install nagios-nrpe-server nagios-plugins nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs nagios-plugins-users
 
 #####NRPE INSTALLATION#####
 yum -y install nrpe
 systemctl enable nrpe
 systemctl start nrpe
+
+
+#####MISC SYSTEM METRICS#####
+#command[check_users]=/usr/lib64/nagios/plugins/check_users $ARG1$
+#command[check_load]=/usr/lib64/nagios/plugins/check_load $ARG1$
+#command[check_disk]=/usr/lib64/nagios/plugins/check_disk $ARG1$
+#command[check_swap]=/usr/lib64/nagios/plugins/check_swap $ARG1$
+#command[check_cpu_stats]=/usr/lib64/nagios/plugins/check_cpu_stats.sh $ARG1$
+
+#####MISC SYSTEM METRICS#####
+command[check_users]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10
+command[check_load]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20
+command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10%
+command[check_swap]=/usr/lib64/nagios/plugins/check_swap -w 20% -c 10%
+command[check_cpu_stats]=/usr/lib64/nagios/plugins/check_cpu_stats.sh -w 70,40,30 -c 90,50,40
 
 #####SERVER NAME#####
 nagios_server="nagios-a"
