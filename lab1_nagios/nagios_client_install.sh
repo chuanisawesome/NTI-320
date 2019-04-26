740a34ffe7b10f25485140ec409c7aa6b401d86a
chuanisawesome <cchang30@seattlecentral.edu>
2019-04-25 18:16:11 -0700

    Update nagios_client_install.sh

--- lab1_nagios/nagios_client_install.sh
+++ lab1_nagios/nagios_client_install.sh
@@ -43,6 +43,15 @@ replacement_string='command\[check_disk\]=/usr/lib/nagios/plugins/check_disk -w 
 # use semicolons as delimiter
 sed -i.bak "s;$string;$replacement_string;g" /etc/nagios/nrpe.cfg
 
+### MISC SYSTEM METRICS ###
+#sed -i 's;#command\[check_users\]=/usr/lib64/nagios/plugins/check_users $ARG1$;command\[check_users\]=/usr/lib64/nagios/plugins/check_users -w 5 -c 10;g' /etc/nagios/nrpe.cfg
+#sed -i 's;#command\[check_load\]=/usr/lib64/nagios/plugins/check_load $ARG1$;command\[check_load\]=/usr/lib64/nagios/plugins/check_load -w 15,10,5 -c 30,25,20;g' /etc/nagios/nrpe.cfg
+#sed -i 's;#command\[check_disk\]=/usr/lib64/nagios/plugins/check_disk $ARG1$;command\[check_disk\]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10%;g' /etc/nagios/nrpe.cfg
+#sed -i 's;#command\[check_swap\]=/usr/lib64/nagios/plugins/check_swap $ARG1$;command\[check_swap\]=/usr/lib64/nagios/plugins/check_swap -w 20% -c 10%;g' /etc/nagios/nrpe.cfg
+#sed -i 's;#command\[check_cpu_stats\]=/usr/lib64/nagios/plugins/check_cpu_stats.sh $ARG1$;command\[check_cpu_stats\]=/usr/lib64/nagios/plugins/check_cpu_stats.sh -w 70,40,30 -c 90,50,40;g' /etc/nagios/nrpe.cfg
+#sed -i 's,#command\[check_mem\]=/usr/lib64/nagios/plugins/custom_check_mem -n $ARG1$,command\[check_mem\]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90,g' /etc/nagios/nrpe.cfg
+
+
 #####SERVER NAME#####
 nagios_server="nagios-a"
 #####INTERNAL IP#####
