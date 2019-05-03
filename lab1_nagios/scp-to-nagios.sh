4c6711a9443d01b2d7ace6b8ed6550bad233fafd
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-02 18:49:51 -0700

    scp file

---
+++ lab1_nagios/scp-to-nagios.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+bash generate_config.sh $1 $2
+
+gcloud compute scp $1.cfg cchang30@nagios-a:/etc/nagios/servers
+
+# Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
+# I also had to chmod 775 /etc/nagios/servers
+#usermod -a -G nagios nicolebade
+
+gcloud compute ssh cchang30@nagios-a --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
