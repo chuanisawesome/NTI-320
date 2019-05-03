998456f2e29e3721cf0c8a646d63f029e613b7a6
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-02 20:27:18 -0700

    Update for-loop-nrpe-install.sh

--- lab1_nagios/for-loop-nrpe-install.sh
+++ lab1_nagios/for-loop-nrpe-install.sh
@@ -1,3 +1,5 @@
 for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a); do 
-gcloud compute ssh --zone us-west1-b cchang30@$servername --command='yum -y install wget && wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab1_nagios/nagios_client_install
-.sh && bash nagios_client_install.sh'
+gcloud compute ssh --zone us-west1-b cchang30@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab1_nagios/nagios_client_install
+.sh && sudo bash nagios_client_install.sh';
+
+done
