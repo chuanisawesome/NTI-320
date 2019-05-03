a91ee41a50c2a65c049a1754ee35f7fae9d23637
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-02 20:03:46 -0700

    Update for-loop.sh

--- lab1_nagios/for-loop.sh
+++ lab1_nagios/for-loop.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" );  do 
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a);  do 
     echo $servername;
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
+    
+    bash scp-to-nagios.sh $servername $serverip
 done
+
+gcloud compute ssh cchang30@nagios-a --command='sudo systemctl restart nagios'
