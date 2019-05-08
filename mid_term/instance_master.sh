96a8ff6eee20ae553dadd2fe30eb5bf86858cd00
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-08 14:18:06 -0700

    install nrpe

--- mid_term/instance_master.sh
+++ mid_term/instance_master.sh
@@ -144,6 +144,15 @@ fi
 
 mv $dir ./ip.sh | /bin/bash
 
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $nagios_server); do 
+
+gcloud compute ssh --zone us-west1-b cchang30@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab1_nagios/nagios_client_install
+.sh && sudo bash nagios_client_install.sh';
+
+done
+
+sleep 10
+
 for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $nagios_server);  do 
     echo $servername;
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
