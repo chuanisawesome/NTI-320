307e91234865e9988797cb921c5898b5c3aea998
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-08 14:54:44 -0700

    Update instance_master.sh

--- mid_term/instance_master.sh
+++ mid_term/instance_master.sh
@@ -146,8 +146,7 @@ mv $dir ./ip.sh | /bin/bash
 
 for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $nagios_server); do 
 
-gcloud compute ssh --zone us-west1-b cchang30@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab1_nagios/nagios_client_install
-.sh && sudo bash nagios_client_install.sh';
+gcloud compute ssh --zone us-west1-b cchang30@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/chuanisawesome/NTI-320/master/lab1_nagios/nagios_client_install.sh && sudo bash nagios_client_install.sh';
 
 done
 
