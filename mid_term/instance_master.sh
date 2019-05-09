3dc6d9edfd9c805bf7d80b6eecc9c13dd4160ffb
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-08 20:35:29 -0700

    Update instance_master.sh

--- mid_term/instance_master.sh
+++ mid_term/instance_master.sh
@@ -145,7 +145,7 @@ for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d"
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
     
-    bash scp-to-nagios.sh $servername $serverip
+    ./scp-to-nagios.sh $servername $serverip
 done
 
 gcloud compute ssh --zone us-west1-b cchang30@$nagios_server --command='sudo systemctl restart nagios'
