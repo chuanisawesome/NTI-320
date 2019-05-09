44d6d0999b8227c29cdb1ec9597c2b5cc5952f1d
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-08 20:17:45 -0700

    Update scp-to-nagios.sh

--- lab1_nagios/scp-to-nagios.sh
+++ lab1_nagios/scp-to-nagios.sh
@@ -2,12 +2,12 @@
 
 bash generate_config.sh $1 $2
 
-gcloud compute scp --zone us-west1-b $1.cfg cchang30@nagios-a:/etc/nagios/servers
+gcloud compute scp --zone us-west1-b $1.cfg cchang30@testingnagios:/etc/nagios/servers
 
 # Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
 # I also had to chmod 775 /etc/nagios/servers
 #usermod -a -G nagios nicolebade
 
-gcloud compute ssh --zone us-west1-b cchang30@nagios-a --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
+gcloud compute ssh --zone us-west1-b cchang30@testingnagios --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
 
 
