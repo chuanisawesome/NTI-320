7022e45386462d24df3819d91edd91846af94ac7
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-08 20:18:52 -0700

    Update for-loop.sh

--- lab1_nagios/for-loop.sh
+++ lab1_nagios/for-loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a);  do 
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v testingnagios );  do 
     echo $servername;
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
