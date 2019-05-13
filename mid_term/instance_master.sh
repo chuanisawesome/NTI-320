915d4eacef457170a03524e51151e88755adc1aa
chuanisawesome <cchang30@seattlecentral.edu>
2019-05-13 15:01:58 -0700

    Update instance_master.sh

--- mid_term/instance_master.sh
+++ mid_term/instance_master.sh
@@ -152,6 +152,8 @@ gcloud compute instances create $django_server \
     --image-project centos-cloud \
     --tags "http-server","django-server" \
     --metadata-from-file startup-script="/NTI-320/mid_term/django-startup-script.sh"
+ 
+ sleep 2
 
 for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v $nagios_server); do 
 
