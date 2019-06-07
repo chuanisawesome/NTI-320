675642f005c81fee2d66497247c5eb1def16432a
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-06 18:36:47 -0700

    updated ldap client

--- mid_term/ldap-nfs-client-startup-script.sh
+++ mid_term/ldap-nfs-client-startup-script.sh
@@ -6,26 +6,46 @@ apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
 # Installs debconf
-apt-get -y install debconf-utils
+apt-get --yes install libnss-ldap libpam-ldap ldap-utils nscd
 
-# echo ldap debconf
-echo -e "ldap-auth-config	ldap-auth-config/rootbindpw	password	
-ldap-auth-config	ldap-auth-config/bindpw	password	
-ldap-auth-config	ldap-auth-config/ldapns/ldap_version	select	3
-ldap-auth-config	ldap-auth-config/pam_password	select	md5
-ldap-auth-config	ldap-auth-config/binddn	string	cn=proxyuser,dc=example,dc=net
-ldap-auth-config	ldap-auth-config/move-to-debconf	boolean	true
-ldap-auth-config	ldap-auth-config/override	boolean	true
-ldap-auth-config	ldap-auth-config/rootbinddn	string	cn=manager,dc=example,dc=net
-ldap-auth-config	ldap-auth-config/ldapns/ldap-server	string	ldap://$ldap_ip
-ldap-auth-config	ldap-auth-config/ldapns/base-dn	string	dc=nti310,dc=local
-ldap-auth-config	ldap-auth-config/dblogin	boolean	false
-ldap-auth-config	ldap-auth-config/dbrootlogin	boolean	false" > ldap_debconf
+# echo ldap tempfile
+echo "ldap-auth-config ldap-auth-config/bindpw password
+nslcd nslcd/ldap-bindpw password
+ldap-auth-config ldap-auth-config/rootbindpw password
+ldap-auth-config ldap-auth-config/move-to-debconf boolean true
+nslcd nslcd/ldap-sasl-krb5-ccname string /var/run/nslcd/nslcd.tkt
+nslcd nslcd/ldap-starttls boolean false
+libpam-runtime libpam-runtime/profiles multiselect unix, ldap, systemd, capability
+nslcd nslcd/ldap-sasl-authzid string
+ldap-auth-config ldap-auth-config/rootbinddn string cn=ldapadm,dc=nti310,dc=local
+nslcd nslcd/ldap-uris string ldap://ldap-d
+nslcd nslcd/ldap-reqcert select
+nslcd nslcd/ldap-sasl-secprops string
+ldap-auth-config ldap-auth-config/ldapns/ldap_version select 3
+ldap-auth-config ldap-auth-config/binddn string cn=proxyuser,dc=example,dc=net
+nslcd nslcd/ldap-auth-type select none
+nslcd nslcd/ldap-cacertfile string /etc/ssl/certs/ca-certificates.crt
+nslcd nslcd/ldap-sasl-realm string
+ldap-auth-config ldap-auth-config/dbrootlogin boolean true
+ldap-auth-config ldap-auth-config/override boolean true
+nslcd nslcd/ldap-base string dc=nti310,dc=local
+ldap-auth-config ldap-auth-config/pam_password select md5
+nslcd nslcd/ldap-sasl-mech select
+nslcd nslcd/ldap-sasl-authcid string
+ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=nti310,dc=local
+ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldap-d/
+nslcd nslcd/ldap-binddn string
+ldap-auth-config ldap-auth-config/dblogin boolean false" >> tempfile
 
-while read line; do echo "$line" | debconf-set-selections; done < ldap_debconf
+while read line; do echo "$line" | debconf-set-selections; done < tempfile
 
-# Installs ldap utils
-apt-get -y install libpam-ldap nscd
+echo "$secretpasswd" > /etc/ldap.secret
+sudo auth-client-config -t nss -p lac_ldap
+
+echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
+sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
+sed -i 's,uri ldapi:///,uri ldap://ldap-d/,g' /etc/ldap.conf
+sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
 
 # Set login to include ldap
 sed -i 's/compat/compat ldap/g' /etc/nsswitch.conf
