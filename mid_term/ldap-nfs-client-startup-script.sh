77facfb427176e79677f2638c42ea4218e921bf1
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-06 20:10:35 -0700

    updated ldap server name

--- mid_term/ldap-nfs-client-startup-script.sh
+++ mid_term/ldap-nfs-client-startup-script.sh
@@ -21,7 +21,7 @@ nslcd nslcd/ldap-starttls boolean false
 libpam-runtime libpam-runtime/profiles multiselect unix, ldap, systemd, capability
 nslcd nslcd/ldap-sasl-authzid string
 ldap-auth-config ldap-auth-config/rootbinddn string cn=ldapadm,dc=nti310,dc=local
-nslcd nslcd/ldap-uris string ldap://ldap-d
+nslcd nslcd/ldap-uris string ldap://testingldap
 nslcd nslcd/ldap-reqcert select
 nslcd nslcd/ldap-sasl-secprops string
 ldap-auth-config ldap-auth-config/ldapns/ldap_version select 3
@@ -36,7 +36,7 @@ ldap-auth-config ldap-auth-config/pam_password select md5
 nslcd nslcd/ldap-sasl-mech select
 nslcd nslcd/ldap-sasl-authcid string
 ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=nti310,dc=local
-ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldap-d/
+ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://testingldap/
 nslcd nslcd/ldap-binddn string
 ldap-auth-config ldap-auth-config/dblogin boolean false" >> tempfile
 
@@ -47,7 +47,7 @@ sudo auth-client-config -t nss -p lac_ldap
 
 echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
 sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
-sed -i 's,uri ldapi:///,uri ldap://ldap-d/,g' /etc/ldap.conf
+sed -i 's,uri ldapi:///,uri ldap://testingldap/,g' /etc/ldap.conf
 sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
 
 systemctl restart nscd
