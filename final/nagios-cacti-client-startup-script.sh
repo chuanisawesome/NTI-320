8d3e8fc43ebbc513a86c124348847c92ddbd67aa
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:47:47 -0700

    Update nagios-cacti-client-startup-script.sh

--- final/nagios-cacti-client-startup-script.sh
+++ final/nagios-cacti-client-startup-script.sh
@@ -76,3 +76,19 @@ systemctl enable snmpd
 systemctl start snmpd
 
 systemctl restart nrpe
+
+###setting up machine to run as rsyslog client to server rsyslog
+yum update -y && yum install -y rsyslog
+
+systemctl enable rsyslog
+systemctl start rsyslog
+
+#on the rsyslog client
+#add to end of file
+#internal ip
+echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+##check to see if rsyslog is active
+systemctl status rsyslog
