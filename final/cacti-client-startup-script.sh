bbd00c47e3a9ee3bd627ab0a34f45d046153f2e4
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:46:42 -0700

    Update cacti-client-startup-script.sh

--- final/cacti-client-startup-script.sh
+++ final/cacti-client-startup-script.sh
@@ -25,3 +25,19 @@ access myGroup    ""    any    noauth     exact    all    all    none' >> /etc/s
 # Enable snmp, Start snmp
 systemctl enable snmpd
 systemctl start snmpd
+
+###setting up machine to run as rsyslog client to server rsyslog
+yum update -y && yum install -y rsyslog
+
+systemctl enable rsyslog
+systemctl start rsyslog
+
+#on the rsyslog client
+#add to end of file
+#internal ip
+echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+##check to see if rsyslog is active
+systemctl status rsyslog
