d71f732ab18b90c752440b1a8f1ced14993f4d5e
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:58:48 -0700

    Update repo-startup-script.sh

--- final/repo-startup-script.sh
+++ final/repo-startup-script.sh
@@ -24,19 +24,3 @@ sed -i 's/^/#/' /etc/httpd/conf.d/welcome.conf
 
 chown -R apache:apache /repos/
 systemctl restart httpd
-
-###setting up machine to run as rsyslog client to server rsyslog
-yum update -y && yum install -y rsyslog
-
-systemctl enable rsyslog
-systemctl start rsyslog
-
-#on the rsyslog client
-#add to end of file
-#internal ip
-echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
-
-systemctl restart rsyslog
-
-##check to see if rsyslog is active
-systemctl status rsyslog
