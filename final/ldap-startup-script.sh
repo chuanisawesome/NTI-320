d7d167fed9f966a8a28a1b7e92a05089ed9f7da6
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:48:00 -0700

    Update ldap-startup-script.sh

--- final/ldap-startup-script.sh
+++ final/ldap-startup-script.sh
@@ -117,3 +117,19 @@ setenforce 0
 ldapadd -x -W -D "cn=ldapadm,dc=nti310,dc=local" -f base.ldif -y /root/ldap_admin_pass
 
 systemctl restart httpd
+
+###setting up machine to run as rsyslog client to server rsyslog
+yum update -y && yum install -y rsyslog
+
+systemctl enable rsyslog
+systemctl start rsyslog
+
+#on the rsyslog client
+#add to end of file
+#internal ip
+echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+##check to see if rsyslog is active
+systemctl status rsyslog
