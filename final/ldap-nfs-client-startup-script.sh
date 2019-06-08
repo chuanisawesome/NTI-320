0a14c72bafd1c652574cca8fc8d826845e3bcf0e
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:47:34 -0700

    Update ldap-nfs-client-startup-script.sh

--- final/ldap-nfs-client-startup-script.sh
+++ final/ldap-nfs-client-startup-script.sh
@@ -80,3 +80,19 @@ mount -a
 
 #change to testing directory
 cd /mnt/test
+
+###setting up machine to run as rsyslog client to server rsyslog
+yum update -y && yum install -y rsyslog
+
+systemctl enable rsyslog
+systemctl start rsyslog
+
+#on the rsyslog client
+#add to end of file
+#internal ip
+echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+##check to see if rsyslog is active
+systemctl status rsyslog
