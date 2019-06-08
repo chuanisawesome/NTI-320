132f7915a150e11e37d7a58bc91da13adf84c5c8
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:58:29 -0700

    Update nagios-startup-script.sh

--- final/nagios-startup-script.sh
+++ final/nagios-startup-script.sh
@@ -66,20 +66,3 @@ command_line $USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
 
 #####RESTART NAGIOS#####
 systemctl restart nagios
-
-
-###setting up machine to run as rsyslog client to server rsyslog
-yum update -y && yum install -y rsyslog
-
-systemctl enable rsyslog
-systemctl start rsyslog
-
-#on the rsyslog client
-#add to end of file
-#internal ip
-echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
-
-systemctl restart rsyslog
-
-##check to see if rsyslog is active
-systemctl status rsyslog
