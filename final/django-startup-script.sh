fcb85d38355817ef037a102322a14fe83f25b946
chuanisawesome <cchang30@seattlecentral.edu>
2019-06-08 14:47:15 -0700

    Update django-startup-script.sh

--- final/django-startup-script.sh
+++ final/django-startup-script.sh
@@ -13,6 +13,22 @@ pip install virtualenv
 pip install --upgrade pip
 yum install -y telnet
 
+###setting up machine to run as rsyslog client to server rsyslog
+yum update -y && yum install -y rsyslog
+
+systemctl enable rsyslog
+systemctl start rsyslog
+
+#on the rsyslog client
+#add to end of file
+#internal ip
+echo "*.* @@$rsys_ip:514" >> /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+##check to see if rsyslog is active
+systemctl status rsyslog
+
 mkdir /opt/myproject
 cd /opt/myproject
 
